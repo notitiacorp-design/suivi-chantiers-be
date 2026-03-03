702b03baa19582ae25c5c1caa48848031f416489
notitiacorp-design <notitiacorp@gmail.com>
2026-03-03 13:44:14 +0100

    Fix desktop content offset to prevent sidebar overlap

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -7,10 +7,10 @@ const MainLayout: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-slate-50 lg:flex">
+    <div className="min-h-screen bg-slate-50">
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       
-      <div className="lg:ml-64 flex flex-col min-h-screen flex-1">
+      <div className="flex flex-col min-h-screen lg:pl-64">
         <Header onMenuClick={() => setSidebarOpen(true)} />
         
         <main className="flex-1 p-4 lg:p-8">
